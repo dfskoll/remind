4ba21ab52629b829f59d0970ae6b4326f7d0b500
David F. Skoll <dfs@roaringpenguin.com>
2012-01-23 12:01:11 -0500

    Fix test-all-languages.sh.

--- src/test-all-languages.sh
+++ src/test-all-languages.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+# Make sure Remind compiles with all supported languages; show
+# tstlang.rem output for each language.
 
 ALL=`grep ^#define lang.h | grep -v '#define LANG' | awk '{print $2}'`
-echo $ALL
 
 for i in $ALL ; do
     make clean all LANGDEF=-DLANG=$i || exit 1
-    ./remind ../tests/tstlang.rem
+    ./remind -q -r ../tests/tstlang.rem
 done
 exit 0

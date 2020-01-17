edbc88bd19b33ed229a242a867df3e3e48d0fafd
Dianne Skoll <dianne@skoll.ca>
2020-01-16 20:00:38 -0500

    Reformat error message.

--- src/rem2ps.c
+++ src/rem2ps.c
@@ -305,7 +305,7 @@ int main(int argc, char *argv[])
     while (!feof(stdin)) {
 	DBufGets(&buf, stdin);
 	if (first_line && (!strcmp(DBufValue(&buf), "["))) {
-	    fprintf(stderr, "It appears that you have invoked Remind with the -ppp option.\nPlease use either -p or -pp, but not -ppp.\n");
+	    fprintf(stderr, "Rem2PS: It appears that you have invoked Remind with the -ppp option.\n        Please use either -p or -pp, but not -ppp.\n");
 	    exit(1);
 	}
 	first_line = 0;

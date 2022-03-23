4fdf7b706b4e2ee4b0a2b64f9b8ea1bb80b02909
Dianne Skoll <dianne@skoll.ca>
2022-03-23 11:47:00 -0400

    More work on making run-time language module match compile-time module.

--- src/compare-language-mods.pl
+++ src/compare-language-mods.pl
@@ -32,12 +32,18 @@ my $flag = $language_map->{$lang};
 print STDERR "Testing for: $lang - $flag.\n";
 my_sys("make clean") && die("make clean failed");
 my_sys("make -j6 all LANGDEF=-DLANG=$flag") && die("make all failed");
-my_sys("./remind -q -r ../tests/tstlang.rem > test-$lang-compiled.out 2>&1");
+my_sys("./remind -q -r ../tests/tstlang.rem 2022-03-23 11:44 > test-$lang-compiled.out 2>&1");
 
 my_sys("make clean") && die("make clean failed");
 my_sys("make -j6 all") && die("make all failed");
-my_sys("./remind -q -r -ii=\\\"../include/lang/$lang.rem\\\" ../tests/tstlang.rem > test-$lang-runtime.out 2>&1");
+my_sys("./remind -q -r -ii=\\\"../include/lang/$lang.rem\\\" ../tests/tstlang.rem 2022-03-23 11:44 > test-$lang-runtime.out 2>&1");
 
+my $rc = my_sys("cmp test-$lang-compiled.out test-$lang-runtime.out > /dev/null 2>&1");
+if ($rc == 0) {
+        print STDERR "Congrats!  Compiled and runtime language output matches for $lang.\n";
+} else {
+        print STDERR "Whoops.  Compiled and runtime language output differs for $lang.\n"
+}
 exit(0);
 
 sub my_sys
